ff6f9728c77b3c853b4b92fbe5ae79138f935935
sarah kiarie <sarahwanjiruki1@gmail.com>
2025-10-01 14:36:19 +0300

    entity framework

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,12 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<GameStoreContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DBConnection")));
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
